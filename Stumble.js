ab0863822670ec4e1ca7720cf04582d3f01c6126
ArjunaGimank <107677200+ArjunaGimank@users.noreply.github.com>
2022-06-18 17:48:08 +0700

    Update Stumble.js

--- Stumble.js
+++ Stumble.js
@@ -5,7 +5,7 @@ const rs = require('readline-sync');
 
 const GoStumble = (auth) => new Promise((resolve, reject) => {
 
-    fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/', {
+    fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/3', {
         method: 'GET',
         headers: {
             'authorization': auth
